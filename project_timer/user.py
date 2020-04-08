6df9af4bebc0578e23f240a41c80453f39e0c514
Christiaan Lamers <Christiaan.Lamers@ordina.nl>
2020-04-08 15:52:54 +0200

    added comments on code

--- project_timer/user.py
+++ project_timer/user.py
@@ -1,7 +1,7 @@
 class User():
     def __init__(self, name: str, id: int):
-        self.name = str(name)
-        self.id = int(id)
+        self.name = str(name)#CHRISTIAAN: Het lijkt me niet nodig om dit naar een string te casten. Het zou al een string moeten zijn.
+        self.id = int(id)#CHRISTIAAN: Het lijkt me ook niet nodig om dit naar een int te casten.
 
     def welcome(self):
         if self.id < 10:

c0fd81ecff79d58c31cdf0dfc084aa7b7196dc42
Mostafa Abd Elrehim <mostafa.abd.elrehim@ordina.nl>
2020-04-10 16:25:07 +0200

    added test, fixtures and adding and removing time entries

--- project_timer/task.py
+++ project_timer/task.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from random import randint
 from typing import List
 
 from project_timer.timer import TimerEntry
@@ -8,13 +9,29 @@ from project_timer.user import User
 @dataclass
 class Task:
     '''Class that stores task specific data'''
-    id: str
     name: str
-    created_by: User
-    entries: List[TimerEntry] = List #CHRISTIAAN: ik denk dat je "= List" moet vervangen door "= []"
+    _created_by: User
+    _id: int = randint(1, 100)
+    entries: List[TimerEntry] = field(default_factory=list)
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def created_by(self) -> User:
+        return self._created_by
+
+    def add_entry(self, entry: TimerEntry) -> None:
+        self.entries.append(entry)
+
+    def remove_entry(self, entry: TimerEntry) -> None:
+        self.entries.remove(entry)
 
 
 if __name__ == "__main__":
     user = User("programmer", 1)
-    task = Task("id", "fix code", user)
+    task = Task("fix code", user)
+    print(task.created_by)
+    task._created_by = User("programmer", 2)  # works and writes to _created_by
     print(task)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,31 @@
+from project_timer.task import Task
 from project_timer.user import User
+from project_timer.timer import TimerEntry
 
 from datetime import datetime, timedelta
 import pytest
+
 
 @pytest.fixture
 def start_time():
     return datetime.utcnow()
 
+
 @pytest.fixture
 def end_time(start_time):
     return start_time + timedelta(minutes=1)
 
+
 @pytest.fixture
 def user():
     return User("Dirck", 3)
+
+
+@pytest.fixture
+def time_entry(start_time, end_time, user):
+    return TimerEntry(start_time, end_time, user)
+
+
+@pytest.fixture
+def task(user):
+    return Task("fix code", user)
